9ac15091423ecb7b8a3f159fef6edcf59752cb38
Nakul1404 <nakulbhangale@gmail.com>
2024-10-09 10:03:54 -0400

    Updated GET functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,16 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  return res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn1=req.params.isbn;
+  let filtered_books = obj.values(books).filter((book) => book.isbn === isbn1);
+  return res.send(filtered_books);
  });
   
 // Get book details based on author
